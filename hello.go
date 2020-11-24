c5296da84c0ee473fc85f7fa2ebbdab17f5261e8
TobiasSoares <tobias.soares@inf.ufrgs.br>
2020-11-24 15:21:50 -0300

    Adição da função swap()
    Foi adicionadad a função swap() que troca duas strings de lugar.

--- hello.go
+++ hello.go
@@ -8,6 +8,11 @@ func add(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
-	fmt.Println(add(42, 13))
+	a, b := "Hello", " World"
+	fmt.Println(a, b)
 }
